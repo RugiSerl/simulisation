885099170ca74afcb72b6250551a012f003f6a24
Guiton1er <129685293+Guiton1er@users.noreply.github.com>
2023-04-03 11:39:43 +0200

    ererer

--- app/Game/Entity/Entity.go
+++ app/Game/Entity/Entity.go
@@ -1,9 +1,17 @@
 package Entity
+import "math/rand"
 
 type Entity struct {
-	Name string
+	Age uint8
 }
 
-func New(){
+func NewEntity() *Entity {
 	
+	e := new(Entity)
+	e.Age = 0 
+}
+
+func (e *Entity) Update() {
+	e.Age += 5
+	if e.Age
 }
