702703d72e64314ba03926f9c86436e2391b4ea3
RugiSerl <77239970+RugiSerl@users.noreply.github.com>
2023-04-07 10:24:42 +0200

    did things

--- app/Game/gameComponents/Map.go
+++ app/Game/gameComponents/Map.go
@@ -1,15 +1,20 @@
 package gameComponents
 
 type Map struct {
+	fieldTexture rl.Texture2d
+	
+
+
 }
 
 // taille d'une cellule de la grille
 const GRID_CELL_SIZE = 24
-const GRID_SIZE = 100
+const GRID_SIZE = 40
 
 func NewMap() *Map {
 
 	m := new(Map)
+	m.fieldTexture = rl.LoadTexture("assets/champ.png")
 
 	return m
 
