9e3899b03f24ebcaf6688ba7672a04d81cd608d7
RugiSerl <cooletouf2@gmail.com>
2023-04-08 13:11:42 +0200

    aa

--- app/graphic/Vector2.go
+++ app/graphic/Vector2.go
@@ -1,5 +1,8 @@
 package graphic
 
+import "math"
+
+//a vector 2 is an object containing two number. It can represent a position on screen, a size, or a movement
 type Vector2 struct {
 	X float32
 	Y float32
@@ -13,6 +16,18 @@ func NewVector2(x float32, y float32) *Vector2 {
 	return v
 }
 
-func (v *Vector2) GetDistance() {
+func (v *Vector2) GetNorm() float32 {
 
+	return float32(math.Sqrt(math.Pow((float64(v.X)), 2) + math.Pow((float64(v.Y)), 2)))
+
+}
+
+func (v *Vector2) Add(otherVector *Vector2) {
+	v.X += otherVector.X
+	v.Y += otherVector.Y
+}
+
+func (v *Vector2) Substract(otherVector *Vector2) {
+	v.X -= otherVector.X
+	v.Y -= otherVector.Y
 }
--- app/graphic/circle.go
+++ app/graphic/circle.go
@@ -1,23 +1,15 @@
 package graphic
 
-import (
-	"math"
-
-	rl "github.com/gen2brain/raylib-go/raylib"
-)
-
 type Circle struct {
-	CenterPosition Vector2
+	CenterPosition *Vector2
 	Radius         float32
 }
 
-// Detect circle collision with mouse
-func DetectCircleMouseCollision(circleCenterX float64, circleCenterY float64, circleRadius float64) bool {
+// detect if two circles overlap
+func (c *Circle) DetectCircleCollision(otherCircle *Circle) bool {
+	distance := c.CenterPosition
+	distance.Substract(otherCircle.CenterPosition)
 
-	x, y := float64(rl.GetMouseX()), float64(rl.GetMouseY())
-	distance := math.Sqrt(math.Pow((x-circleCenterX), 2) + math.Pow((y-circleCenterY), 2))
-	rl.NewVector2(0, 0)
-
-	return (distance <= circleRadius)
+	return (distance.GetNorm() <= c.Radius+otherCircle.Radius)
 
 }
