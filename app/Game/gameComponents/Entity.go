b1923d595f7a9d09a827ec9a26ff8a4f2fcd7ea6
RugiSerl <cooletouf7@gmail.com>
2023-04-12 10:33:31 +0200

    test

--- app/Game/gameComponents/Entity.go
+++ app/Game/gameComponents/Entity.go
@@ -1,8 +1,6 @@
 package gameComponents
 
 import (
-	"log"
-
 	"github.com/RugiSerl/simulisation/app/graphic"
 	"github.com/RugiSerl/simulisation/app/math"
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -25,8 +23,6 @@ type Entity struct {
 
 	Position graphic.Vector2 // cordonnées de l'entité sur la map
 
-	Voisins      []*Entity // Tableau des voisins avec lequels l'entité est en lien
-	LiensVoisins []uint8   // Tableau des forces de liaisons avec les voisins
 }
 
 // Initialisation d'une instance entité
@@ -53,14 +49,7 @@ func (e *Entity) Update() {
 // Cette fonction s'occupe d'afficher visuellement l'entité
 func (e *Entity) render() {
 	rl.DrawTextureEx(TextureEntite, rl.Vector2(e.Position), 0, SCALE, rl.White)
-	e.renderLiens()
 
-}
-
-func (e *Entity) renderLiens() {
-	for _, voisin := range e.Voisins {
-		rl.DrawLineEx(voisin.Position.ToRaylibVector2(), e.Position.ToRaylibVector2(), 5, rl.Black)
-	}
 }
 
 // la valeur morale est "cyclique", ce qui signifie que celle entre 5 et 254 est 6 par exemple
@@ -71,24 +60,5 @@ func (e *Entity) DistanceMorale(otherEntity *Entity) uint8 {
 	}
 
 	return distance
-
-}
-
-// création d'un lien avec une autre entité
-func (e *Entity) NouveauLien(entiteVoisine *Entity) {
-	if len(e.Voisins) < NB_VOISINS_MAX {
-		forceDuLien := 128 - e.DistanceMorale(entiteVoisine)
-
-		e.Voisins = append(e.Voisins, entiteVoisine)
-		e.LiensVoisins = append(e.LiensVoisins, forceDuLien)
-
-		entiteVoisine.Voisins = append(entiteVoisine.Voisins, e)
-		entiteVoisine.LiensVoisins = append(entiteVoisine.LiensVoisins, forceDuLien)
-
-		e.Position = entiteVoisine.Position
-		e.Position.Y += 50
-	} else {
-		log.Fatal("l'entité a déjà atteint le nombre maximal de voisins")
-	}
 
 }
