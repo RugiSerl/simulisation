f3c656867da8d12a7abed7245839b84b94b554ca
Guiton1er <129685293+Guiton1er@users.noreply.github.com>
2023-04-12 10:21:04 +0200

    def

--- app/Game/gameComponents/Entity.go
+++ app/Game/gameComponents/Entity.go
@@ -24,9 +24,6 @@ type Entity struct {
 	ValeurMorale uint8 // Valeur aléatoire qui va déterminer le groupe que l'entité rejoindra
 
 	Position graphic.Vector2 // cordonnées de l'entité sur la map
-
-	Voisins      []*Entity // Tableau des voisins avec lequels l'entité est en lien
-	LiensVoisins []uint8   // Tableau des forces de liaisons avec les voisins
 }
 
 // Initialisation d'une instance entité
@@ -53,14 +50,7 @@ func (e *Entity) Update() {
 // Cette fonction s'occupe d'afficher visuellement l'entité
 func (e *Entity) render() {
 	rl.DrawTextureEx(TextureEntite, rl.Vector2(e.Position), 0, SCALE, rl.White)
-	e.renderLiens()
 
-}
-
-func (e *Entity) renderLiens() {
-	for _, voisin := range e.Voisins {
-		rl.DrawLineEx(voisin.Position.ToRaylibVector2(), e.Position.ToRaylibVector2(), 5, rl.Black)
-	}
 }
 
 // la valeur morale est "cyclique", ce qui signifie que celle entre 5 et 254 est 6 par exemple
@@ -74,21 +64,3 @@ func (e *Entity) DistanceMorale(otherEntity *Entity) uint8 {
 
 }
 
-// création d'un lien avec une autre entité
-func (e *Entity) NouveauLien(entiteVoisine *Entity) {
-	if len(e.Voisins) < NB_VOISINS_MAX {
-		forceDuLien := 128 - e.DistanceMorale(entiteVoisine)
-
-		e.Voisins = append(e.Voisins, entiteVoisine)
-		e.LiensVoisins = append(e.LiensVoisins, forceDuLien)
-
-		entiteVoisine.Voisins = append(entiteVoisine.Voisins, e)
-		entiteVoisine.LiensVoisins = append(entiteVoisine.LiensVoisins, forceDuLien)
-
-		e.Position = entiteVoisine.Position
-		e.Position.Y += 50
-	} else {
-		log.Fatal("l'entité a déjà atteint le nombre maximal de voisins")
-	}
-
-}
