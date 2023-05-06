c3b50d787f0c3bc3f8830ec0e27d73c9d94ac961
RugiSerl <cooletouf2@gmail.com>
2023-05-06 13:37:10 +0200

    az

--- app/settings/settings.go
+++ app/settings/settings.go
@@ -11,17 +11,27 @@ type VisualSettings struct {
 }
 
 type EntitySettings struct {
+	// écart de différence morale maximum entre une entité et son enfant
 	ChildMaximumDifference      uint8
+
+	// rayon dans lequel une entité "voit" les autres entités
 	RadiusSensivity             float32
+
+	// durée de vie d'une entité, en s
 	MaximumAge                  float32
+
+	//probabilité qu'une entité se reproduise s'il y a une seule autre entité
 	BaseProbabilityReproduction float32
 }
 
 type Gamerule struct {
+	UpdateAge bool
+	Uncollide bool
+	Reproduce bool
+	move bool
 }
 
 var (
-	GameSettings Settings
 	GameSettings Settings
 )
 
